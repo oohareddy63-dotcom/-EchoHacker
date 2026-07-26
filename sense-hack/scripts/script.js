7c5fa8c1b47f500436db3c1142efd631ede69843
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-07-26 11:52:21 +0530

    Add game over messages for player and hacker
    Added winner messages for game over scenarios.

--- sense-hack/scripts/script.js
+++ sense-hack/scripts/script.js
@@ -2,6 +2,7 @@ var audioObj = new Audio("sound.mp3");
 // Set starting life totals here
 var playerLife = 5;
 var hackerLife = 5;
+
 // Message when the game is over
 var hackerWinnerMessage = "Game over: You got hacked!";
 var playerWinnerMessage = "You defeated the hacker!";
